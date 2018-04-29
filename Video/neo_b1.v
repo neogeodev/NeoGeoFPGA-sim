28707964d1ef1e58f372a7c6b64e9ca7d9fffeec
Furrtek <furrtek@gmail.com>
2018-04-29 01:31:18 +0100

    Grouped repeated stuff in B1 to linebuffer instance

--- Video/neo_b1.v
+++ Video/neo_b1.v
@@ -37,7 +37,7 @@ module neo_b1(
 	input CK2,
 	input CK3,
 	input CK4,
-	input TMS0,					// LB flip, watchdog ?
+	input TMS0,					// LB flip
 	input LD1, LD2,			// Load LB addresses
 	input SS1, SS2,			// Buffer select for render/clearing
 	input S1H1,					// 3MHz offset from CLK_1MB
@@ -59,68 +59,24 @@ module neo_b1(
 	reg [7:0] FIXD_REG;
 	wire [3:0] FIX_COLOR;
 	wire [3:0] SPR_COLOR;
+	wire [11:0] RAMBL_OUT;
+	wire [11:0] RAMBR_OUT;
+	wire [11:0] RAMTL_OUT;
+	wire [11:0] RAMTR_OUT;
+	reg [3:0] FIX_PAL_REG;
+	wire [7:0] SPR_PAL;
+	wire [1:0] MUX_BA;
 	wire [11:0] PA_MUX_A;
 	wire [11:0] PA_MUX_B;
 	wire [11:0] RAM_MUX_OUT;
 	reg [11:0] PA_VIDEO;
-	wire [1:0] MUX_BA;
-	// Line buffers address and data
-	wire [7:0] RAMTL_ADDR;
-	wire [7:0] RAMTR_ADDR;
-	wire [7:0] RAMBL_ADDR;
-	wire [7:0] RAMBR_ADDR;
-	wire [11:0] RAMTL_WR;
-	wire [11:0] RAMTR_WR;
-	wire [11:0] RAMBL_WR;
-	wire [11:0] RAMBR_WR;
-	wire [11:0] RAMTL_RD;
-	wire [11:0] RAMTR_RD;
-	wire [11:0] RAMBL_RD;
-	wire [11:0] RAMBR_RD;
-	// Delay registers
-	reg [3:0] FIX_PAL_REG;
-	reg [7:0] TL_PAL_REG;
-	reg [7:0] TR_PAL_REG;
-	reg [7:0] BL_PAL_REG;
-	reg [7:0] BR_PAL_REG;
-	// Buffer address counters
-	wire [7:0] MUX_A;
-	reg [7:0] COUNTER_A;
-	wire [7:0] MUX_B;
-	reg [7:0] COUNTER_B;
-	wire [7:0] MUX_C;
-	reg [7:0] COUNTER_C;
-	wire [7:0] MUX_D;
-	reg [7:0] COUNTER_D;
-	wire [3:0] GAD_GATED_A;
-	wire [3:0] GBD_GATED_A;
-	wire [3:0] GAD_GATED_B;
-	wire [3:0] GBD_GATED_B;
-	// Debug
-	reg [7:0] LATCH_A;
-	reg [7:0] LATCH_B;
-	reg [7:0] LATCH_C;
-	reg [7:0] LATCH_D;
 	
-	parameter TEST_MODE = 0;	// "XMM" pin
+	//parameter TEST_MODE = 0;	// "XMM" pin
 	
 	initial
 		nCPU_ACCESS <= 1'b1;		// Only useful when the 68k is disabled
 	
 	assign nHALT = nRESET;		// Yup (also those are open-collector)
-
-	assign RAMBL_ADDR = LATCH_A;	// Checked OK
-	assign RAMBR_ADDR = LATCH_B;	// Checked OK
-	assign RAMTL_ADDR = LATCH_C;	// Checked OK
-	assign RAMTR_ADDR = LATCH_D;	// Checked OK
-	assign RAMBL_WE = ~WE1;
-	assign RAMBR_WE = ~WE2;
-	assign RAMTL_WE = ~WE3;
-	assign RAMTR_WE = ~WE4;
-	assign RAMBL_RE = ~RAMBL_WE;
-	assign RAMBR_RE = ~RAMBR_WE;
-	assign RAMTL_RE = ~RAMTL_WE;
-	assign RAMTR_RE = ~RAMTR_WE;
 
 	// 2px fix data reg
 	// BEKU AKUR...
@@ -137,42 +93,19 @@ module neo_b1(
 	// GETU FUCA...
 	always @(posedge PCK1)
 		FIX_PAL_REG <= PBUS[19:16];
-	
-	
-	// MOZA MAKO...
-	assign GAD_GATED_A = GAD | {4{SS2}};
-	// MAPE MUCA...
-	assign RAMTL_WR[3:0] = TEST_MODE ? GBD : GAD_GATED_A;
-	
-	// NEGA NACO...
-	assign GBD_GATED_A = GBD | {4{SS1}};
-	// NOFA NYKO...
-	assign RAMBR_WR[3:0] = TEST_MODE ? GBD : GBD_GATED_A;
 
-	// NUDE NOSY...
-	assign GAD_GATED_B = GAD | {4{SS1}};
-	// NODO NUJA...
-	assign RAMBL_WR[3:0] = TEST_MODE ? GBD : GAD_GATED_B;
+	// BL: WE1 CK1 LD1 SS1 GAD|SS1
+	// BR: WE2 CK2 LD1 SS1 GBD|SS1
+	// TL: WE3 CK3 LD2 SS2 GAD|SS2
+	// TR: WE4 CK4 LD2 SS2 GBD|SS2
+	assign SPR_PAL = PBUS[23:16];
 	
-	// NUDE NOSY...
-	assign GBD_GATED_B = GBD | {4{SS2}};
-	// LANO LODO...
-	assign RAMTR_WR[3:0] = TEST_MODE ? GBD : GBD_GATED_B;
+	linebuffer RAMBL(CK1, WE1, LD1, SS1, GAD, PCK2, SPR_PAL, PBUS[7:0], RAMBL_OUT);
+	linebuffer RAMBR(CK2, WE2, LD1, SS1, GBD, PCK2, SPR_PAL, PBUS[15:8], RAMBR_OUT);
+	linebuffer RAMTL(CK3, WE3, LD2, SS2, GAD, PCK2, SPR_PAL, PBUS[7:0], RAMTL_OUT);
+	linebuffer RAMTR(CK4, WE4, LD2, SS2, GBD, PCK2, SPR_PAL, PBUS[15:8], RAMTR_OUT);
 	
-	
-	
-	always @(posedge PCK2)
-	begin
-		BL_PAL_REG <= PBUS[23:16];		// MANA NAKA...
-		BR_PAL_REG <= PBUS[23:16];		// MESY NEPA...
-		TL_PAL_REG <= PBUS[23:16];		// JETU JUMA...
-		TR_PAL_REG <= PBUS[23:16];		// GENA HARU...
-	end
-	
-	assign RAMBL_WR[11:4] = BL_PAL_REG | {8{SS1}};	// MORA NOKU...
-	assign RAMBR_WR[11:4] = BR_PAL_REG | {8{SS1}};	// MECY NUXA...
-	assign RAMTL_WR[11:4] = TL_PAL_REG | {8{SS2}};	// JEZA JODE...
-	assign RAMTR_WR[11:4] = TR_PAL_REG | {8{SS2}};	// GUSU HYKU...
+	assign MUX_BA = {TMS0, S1H1};
 	
 	/*
 	assign VORU = S1H1 & TMS0;
@@ -180,81 +113,25 @@ module neo_b1(
 	assign VEZA = ~S1H1 & TMS0;
 	assign VOKE = ~S1H1 & ~TMS0;
 	*/
-
-	// Load/count select
-	// RUFY QAZU...
-	assign MUX_A = LD1 ? (COUNTER_A + 1) : PBUS[7:0];
-
-	// Address counter update
-	// REVA QEVU...
-	always @(posedge CK1)
-		COUNTER_A <= MUX_A;
-	
-	// NACY OKYS...
-	always @(*)
-		if (WE1) LATCH_A <= COUNTER_A;
-
-	// Load/count select
-	// PECU QUNY...
-	assign MUX_B = LD1 ? (COUNTER_B + 1) : PBUS[15:8];
-
-	// Address counter update
-	// PAJE QATA...
-	always @(posedge CK2)
-		COUNTER_B <= MUX_B;
-	
-	// PEXU QUVU...
-	always @(*)
-		if (WE2) LATCH_B <= COUNTER_B;
-	
-	// Load/count select
-	// BAME CUNU...
-	assign MUX_C = LD2 ? (COUNTER_C + 1) : PBUS[7:0];
-
-	// Address counter update
-	// BEWA CENA...
-	always @(posedge CK3)
-		COUNTER_C <= MUX_C;
-	
-	// ERYV ENOG...
-	always @(*)
-		if (WE3) LATCH_C <= COUNTER_C;
-	
-	// Load/count select
-	// EGED DUGA...
-	assign MUX_D = LD2 ? (COUNTER_D + 1) : PBUS[15:8];
-
-	// Address counter update
-	// EPAQ DAFU...
-	always @(posedge CK4)
-		COUNTER_D <= MUX_D;
-	
-	// EDYZ ASYX...
-	always @(*)
-		if (WE4) LATCH_D <= COUNTER_D;
-	
-	
-	// JAGU JURA...
-	always @(negedge nAS)
-		nCPU_ACCESS <= A23I | ~A22I;
-	
-	// Note: nRESET is sync'd to frame start
-	watchdog WD(nLDS, RW, A23I, A22I, M68K_ADDR_U, BNKB, nHALT, nRESET, nRST);
-	
-	linebuffer RAMBL(RAMBL_ADDR, RAMBL_WR, RAMBL_RD, RAMBL_RE, RAMBL_WE);
-	linebuffer RAMBR(RAMBR_ADDR, RAMBR_WR, RAMBR_RD, RAMBR_RE, RAMBR_WE);
-	linebuffer RAMTL(RAMTL_ADDR, RAMTL_WR, RAMTL_RD, RAMTL_RE, RAMTL_WE);
-	linebuffer RAMTR(RAMTR_ADDR, RAMTR_WR, RAMTR_RD, RAMTR_RE, RAMTR_WE);
-	
-	assign MUX_BA = {TMS0, S1H1};
 	
 	// Output buffer select
 	// MEGA MAKA MEJU ORUG...
 	assign RAM_MUX_OUT = 
-							(MUX_BA == 2'b00) ? RAMBR_RD :
-							(MUX_BA == 2'b01) ? RAMBL_RD :
-							(MUX_BA == 2'b10) ? RAMTR_RD :
-							RAMTL_RD;
+							(MUX_BA == 2'b00) ? RAMBR_OUT :
+							(MUX_BA == 2'b01) ? RAMBL_OUT :
+							(MUX_BA == 2'b10) ? RAMTR_OUT :
+							RAMTL_OUT;
+	
+	// Priority for palette address bus (PA):
+	// -CPU over everything else
+	// -CHBL (h-blank)
+	// -Fix pixel if opaque
+	// -Line buffers (sprites) ouput
+	
+	// CPU palette RAM access decode
+	// JAGU JURA...
+	always @(negedge nAS)
+		nCPU_ACCESS <= A23I | ~A22I;
 	
 	// Fix/Sprite/Blanking select
 	// KUQA KUTU JARA...
@@ -265,12 +142,11 @@ module neo_b1(
 	always @(posedge CLK_6MB)
 		PA_VIDEO <= PA_MUX_B;
 	
-	// Priority for palette address bus (PA):
-	// -CPU over everything else (?)
-	// -CHBL (priority over CPU ?)
-	// -Fix pixel if opaque
-	// -Line buffer (sprites) output is last
 	// KUTE KENU...
 	assign PA = nCPU_ACCESS ? PA_VIDEO : M68K_ADDR_L;
 
+
+	// Note: nRESET is sync'd to frame start
+	watchdog WD(nLDS, RW, A23I, A22I, M68K_ADDR_U, BNKB, nHALT, nRESET, nRST);
+	
 endmodule
